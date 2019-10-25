3a5635492e5ffd055ba825c67b77d32f8952058c
Lucasmq <lucasmedeirosq@gmail.com>
2019-10-24 23:12:08 -0300

    Autenticacao e validacao

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+const verify = promisify(jwt.verify);
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await verify(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.json(401).json({ error: 'Token invalid' });
+  }
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '500632c50696754bb4cda125f937e7a8',
+  expiresIn: '7d',
+};
